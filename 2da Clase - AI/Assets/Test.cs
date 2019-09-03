578fe1b89597c490faf02ec38305926587ff9e08
PaltavsAwacate <gabriel.jmrg@gmail.com>
2019-09-02 21:26:51 -0500

    Clase 2 - Vectores

---
+++ 2da Clase - AI/Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    Vector3 velocity = new Vector3(-5,4,0);
+    Vector3 acceleration =  new Vector3(0,-9.8f,0);
+   
+    void Update()
+    {   
+        transform.position += velocity * Time.deltaTime;
+        velocity += acceleration * Time.deltaTime;
+
+        if(transform.position.y <= -5){
+           velocity.y *= -1;}
+
+        if(transform.position.x < -9 || transform.position.x > 9  ){
+            velocity.x *= -1;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AI
+
 Clase Lunes
